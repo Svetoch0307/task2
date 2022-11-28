19f8ba6771bbab675c74519969670dd7f053410a
Svetlana0307 <d.svetoch@gmail.com>
2022-11-28 16:04:39 +0300

    Init

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.myapplication
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.example.myapplication.view.SecondActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun onNextActivity(view:View){
+        val intentToCallNextActivity = Intent(this, SecondActivity::class.java)
+        startActivity(intentToCallNextActivity)
+    }
+}
